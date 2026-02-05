49fe224248a50985a1a64c250840fa439af936d7
bank5920th-hash <bank5920th@gmail.com>
2026-02-05 16:54:49 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -108,7 +108,7 @@ import urllib.parse
 mongo_username = urllib.parse.quote_plus('bank')
 mongo_password = urllib.parse.quote_plus('Phurinut1234')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://bank:{mongo_password}@bank-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@bank-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 MONGO_DB_NAME = 'django_store_reviews'
 
 # Password validation
