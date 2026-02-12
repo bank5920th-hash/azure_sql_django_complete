52a82559aeb54cebea07899ad25f1b3b5d3b0612
bank5920th-hash <bank5920th@gmail.com>
2026-02-12 17:11:57 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -97,6 +97,7 @@ DATABASES = {
         'PORT': '1433',
         'OPTIONS': {
             'driver': 'ODBC Driver 18 for SQL Server',
+            'extra_params': 'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;',
         },
     },
 }
